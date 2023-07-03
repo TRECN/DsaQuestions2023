14e028000096bd121e96efd66290902428d56fd9
TRECN <rishabh1103@outlook.com>
2023-07-03 23:06:44 +0530

    day 37 Single Element in a Sorted Arr

---
+++ day 37/SingleElementInSortedArr.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    return 0;
+}
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
+    }
+};
