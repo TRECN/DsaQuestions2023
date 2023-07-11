15fba2b3e27a75dc935e7234062adb5ca2b53e7e
TRECN <rishabh1103@outlook.com>
2023-07-11 19:12:23 +0530

    day 45 Same tree or not

---
+++ day 45/SameTree.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/same-tree/description/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    return 0;
+}
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p||!q)
+            return (p==q);
+
+        return (p->val==q->val)&& isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);   
+    }
+};
