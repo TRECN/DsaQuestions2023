827af7b554272028553d9a01bd5f5f973deb9721
TRECN <rishabh1103@outlook.com>
2023-07-25 19:49:17 +0530

    day 59 Construct BST from preorder

---
+++ day 59/ConstructBSTpre.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/submissions/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    return 0;
+}
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i=0;
+        return build(preorder,i,INT_MAX);
+
+    }
+    TreeNode* build(vector<int>&A,int &i,int bound){
+        if(i==A.size()||A[i]>bound)
+        return NULL;
+        TreeNode* root=new TreeNode(A[i++]);
+        root->left=build(A,i,root->val);
+        root->right=build(A,i,bound);
+        return root;
+    }
+};
