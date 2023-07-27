509161696c42c529e36fcc5754926a54880942a8
TRECN <rishabh1103@outlook.com>
2023-07-27 20:18:05 +0530

    day 61 UniqueBinarySearchTrees DP

---
+++ day 61/UniqueBinarySearchTree.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/unique-binary-search-trees/description/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    return 0;
+}
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int>dp(n+1);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;++j){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+};
