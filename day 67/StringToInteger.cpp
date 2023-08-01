26f6c92bb2b04e0b3c6ea7873a3de6008417418a
TRECN <rishabh1103@outlook.com>
2023-08-01 18:37:47 +0530

    day 67 string to integer

---
+++ day 67/StringToInteger.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/string-to-integer-atoi/description/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    return 0;
+}
+class Solution {
+public:
+    int myAtoi(string s) {
+        stringstream str(s);
+        int i=0;
+        str>>i;
+        return i;
+
+    }
+};
