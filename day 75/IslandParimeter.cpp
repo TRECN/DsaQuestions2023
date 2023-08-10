3fb65b5d9c928a5f7d7f7964b5ec14687bd58129
TRECN <rishabh1103@outlook.com>
2023-08-10 18:18:42 +0530

    day 75 Island Parimeter DFS

---
+++ day 75/IslandParimeter.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/island-perimeter/description/
+cpp
+class Solution {
+public:
+int ans=0;
+    void dfs(vector<vector<int>>&grid,int i,int j){
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]==0){
+            ans++;
+            return;
+        }
+        if(grid[i][j]==-1)return;
+        grid[i][j]=-1;
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+    int islandPerimeter(vector<vector<int>>& grid) {
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1)dfs(grid,i,j);
+            }
+        }
+        return ans;
+    }
+};
