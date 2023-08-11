dbaa227580621a5a761d19a1c96142168654e0cb
TRECN <rishabh1103@outlook.com>
2023-08-11 20:10:53 +0530

    day 75 Maximum Number of Jumps to Reach the last index DP

---
+++ day 76/MaximumNumberOfJumpsToReachTheEnd.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/description/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    return 0;
+}
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+  int n = nums.size();
+    vector<int> dp(n, -1); 
+    
+    dp[0] = 0; 
+    
+    for (int i = 1; i < n; ++i) {
+        for (int j = i - 1; j >= 0 && j >= i - target; --j) {
+            if (abs(nums[i] - nums[j]) <= target) {
+                if (dp[i] == -1 || dp[j] + 1 > dp[i]) {
+                    dp[i] = max(dp[i],dp[j] + 1);
+                }
+            }
+        }
+    }
+    
+    return dp[n - 1];
+    }
+};
