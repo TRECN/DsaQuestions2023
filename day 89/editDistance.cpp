794c672882c979fc2bc5a4aaaf7ee1d1fce6caed
TRECN <rishabh1103@outlook.com>
2023-08-24 20:48:07 +0530

    day 89 edit Distance

---
+++ day 89/editDistance.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/edit-distance/description/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    return 0;
+}
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++)
+            dp[i][0]=i;
+        for(int j=0;j<=m;j++)
+            dp[0][j]=j;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){    
+                dp[i][j]=(word1[i-1]==word2[j-1])?dp[i-1][j-1]:1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+            }
+        }
+        return dp[n][m];
+    }
+};
